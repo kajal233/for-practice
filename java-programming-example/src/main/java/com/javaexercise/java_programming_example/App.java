5d78b7216e97bb4f3e40a7b6ad5fbc1c23889867
kajal233 <kajaligdtu@gmail.com>
2021-12-10 18:34:44 +0530

    updated

---
+++ java-programming-example/src/main/java/com/javaexercise/java_programming_example/App.java
@@ -0,0 +1,13 @@
+package com.javaexercise.java_programming_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
